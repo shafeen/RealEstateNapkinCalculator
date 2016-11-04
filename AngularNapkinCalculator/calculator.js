7051c1a415f5fe8f3948331292189f1ea4d7613c
Shafeen M <sodl02@gmail.com>
2016-11-04 01:44:11 -0400

    updated AngularNapkinCalculator: splitting up the views with ngRoute

--- AngularNapkinCalculator/calculator.js
+++ AngularNapkinCalculator/calculator.js
@@ -1,4 +1,16 @@
-angular.module('napkinCalculatorApp', [])
+angular.module('napkinCalculatorApp', ["ngRoute"])
+.config(function ($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "calculator.html"
+    })
+    .when("/calculator", {
+        templateUrl: "calculator.html"
+    })
+    .when("/compare", {
+        templateUrl: "compare.html"
+    });
+})
 .controller('NapkinCalcCtrl', function () {
     var calculator = this;
 
