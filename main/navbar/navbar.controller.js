9894d0e811a0d8aebab138cb1e6b353c7adf7344
Shafeen M <sodl02@gmail.com>
2016-11-24 13:49:24 -0500

    updated navbar controller and view with login functions

--- main/navbar/navbar.controller.js
+++ main/navbar/navbar.controller.js
@@ -1,5 +1,29 @@
 angular.module('napkinCalculatorApp')
-.controller('NavbarCtrl', function ($location) {
+.controller('NavbarCtrl', function ($http, $location, $window, $timeout) {
     var navbar = this;
+
     navbar.$location = $location;
+    navbar.$window = $window;
+
+    const LOGIN_URL = '/login';
+    const LOGIN_SUCCESS_URL = '/dashboard';
+    navbar.login = function() {
+        var loginParams = {
+            email: $('#email').val(),
+            password: $('#password').val()
+        };
+        $http.post(LOGIN_URL, loginParams)
+        .then(function success() {
+            $window.location.href = LOGIN_SUCCESS_URL;
+        }, function failure() {
+            $('#errorPanel').show().find('.panel-body').text('Login failed');
+            $timeout(function () {
+                $('#errorPanel').hide();
+            }, 3000)
+        });
+    };
+
+    navbar.clicked = function () {
+        alert('test click succeeded!')
+    };
 });
