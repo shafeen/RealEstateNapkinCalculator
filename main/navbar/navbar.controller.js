f8088b72216f60fbebc64dfcd49025736c53934d
Shafeen M <sodl02@gmail.com>
2016-11-28 01:23:14 -0500

    updated navbar signup: added error handers for email/password

--- main/navbar/navbar.controller.js
+++ main/navbar/navbar.controller.js
@@ -28,6 +28,12 @@ angular.module('napkinCalculatorApp')
     navbar.signup = function () {
         // TODO: complete this -> create and bind verification functions
         console.log('attempting to sign up!');
+
+        if (verifySignupParams() == false) {
+            console.log('there were errors in the signup params!');
+            return;
+        }
+
         var signupParams = {
             email: $('#signup-email').val(),
             password: $('#signup-password').val()
@@ -36,16 +42,75 @@ angular.module('napkinCalculatorApp')
             .then(function success() {
                 $window.location.href = SIGNUP_SUCCESS_URL;
             }, function failure() {
-                $('#signup-errorPanel').show().find('.panel-body').text('Signup failed');
-                $timeout(function () {
-                    $('#signup-errorPanel').hide();
-                }, 4000)
+                setSignupErrorMsg('Signup failed', true);
             });
     };
 
-    navbar.verifySignup = function() {
-        // TODO: complete this
-    };
+    function verifySignupParams() {
+        // TODO: complete this and verify email and password here ---> maybe do it the angular way
+
+        // TODO: verify email here
+        var emailValid = validateEmail($('#signup-email').val());
+        if (!emailValid) {
+            invalidEmailHandler('You must enter a valid email address.');
+            return false;
+        }
+
+        var passwordsDontMatch = $('#signup-password').val() != $('#signup-password-confirm').val();
+        var passwordValid = passwordsDontMatch ? false : validatePassword($('#signup-password').val());
+        if (passwordsDontMatch) {
+            passwordErrorHandler('Your passwords must match.');
+            return false;
+        }
+        if (!passwordValid) {
+            passwordErrorHandler('Your password must be at least 8 characters long.');
+            return false;
+        }
+        return true;
+    }
+
+    function validateEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+
+    function validatePassword(password) {
+        const MIN_LENGTH = 8;
+        // TODO: add a few more password rules (min length, alphanumeric requirement, etc)
+        if (password.length < MIN_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
+    function invalidEmailHandler(message) {
+        $('#signup-email').parent().addClass('has-error');
+        setSignupErrorMsg(message, true);
+        $timeout(function () {
+            $('#signup-email').parent().removeClass('has-error');
+        }, 4000);
+    }
+
+    function passwordErrorHandler(message) {
+        $('#signup-password-confirm').parent().addClass('has-error');
+        $('#signup-password').parent().addClass('has-error');
+        setSignupErrorMsg(message, true);
+        $timeout(function () {
+            $('#signup-password-confirm').parent().removeClass('has-error');
+            $('#signup-password').parent().removeClass('has-error');
+        }, 4000);
+    }
+
+    function setSignupErrorMsg(message, hideAfterwards) {
+        $('#signup-errorPanel').show().find('.panel-body').text(message);
+        if (hideAfterwards) {
+            $timeout(function () {
+                $('#signup-errorPanel').hide();
+            }, 4000);
+        }
+
+    }
+
 
     navbar.clicked = function () {
         console.log('test click succeeded!');
